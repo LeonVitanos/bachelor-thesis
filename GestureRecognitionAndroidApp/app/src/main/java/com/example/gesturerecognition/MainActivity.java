835dcebd627c2a5782f0bd95673ce46aa04c83e2
Leon Vitanos <leon.vitanos@gmail.com>
2019-09-01 23:48:44 +0300

    improved collection of sensor data, to make the sampling rate ~10HZ

--- GestureRecognitionAndroidApp/app/src/main/java/com/example/gesturerecognition/MainActivity.java
+++ GestureRecognitionAndroidApp/app/src/main/java/com/example/gesturerecognition/MainActivity.java
@@ -17,6 +17,7 @@ import android.content.res.AssetFileDescriptor;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.lang.Math;
 
 import static java.lang.Math.sqrt;
 
@@ -31,7 +32,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     ArrayList<Float> x = new ArrayList<Float>();
     ArrayList<Float> y = new ArrayList<Float>();
     ArrayList<Float> z = new ArrayList<Float>();
-    long previous_time_stamp=0;
+
+    long last_time_stamp = 0;
+    long last_time_stamp_added = 0;
+    float temp_x = 0;
+    float temp_y = 0;
+    float temp_z = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +79,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     SM.unregisterListener(MainActivity.this);
                     button.setText("Start");
                     recordingXYZ = false;
+                    last_time_stamp_added=0;
 
                     xText.setVisibility(View.INVISIBLE);
                     yText.setVisibility(View.INVISIBLE);
@@ -132,7 +139,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     gestureNumber.setVisibility(View.VISIBLE);
                 }
                 else {
-                    SM.registerListener(MainActivity.this, mySensor, 90000);
+                    SM.registerListener(MainActivity.this, mySensor, SensorManager.SENSOR_DELAY_GAME);
                     button.setText("Stop");
                     recordingXYZ = true;
 
@@ -163,19 +170,48 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         return gesture;
     }
 
+    public void addSensorData(float x_axis, float y_axis, float z_axis){
+        xText.setText("X: " + x_axis);
+        x.add(x_axis);
+        yText.setText("Y: " + y_axis);
+        y.add(y_axis);
+        zText.setText("Z: " + z_axis);
+        z.add(z_axis);
+        count += 1;
+        System.out.println(System.currentTimeMillis());
+    }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         long currentTimeMillis = System.currentTimeMillis();
-        if(recordingXYZ && currentTimeMillis-previous_time_stamp>40) {
-            xText.setText("X: " + sensorEvent.values[0]);
-            x.add(sensorEvent.values[0]);
-            yText.setText("Y: " + sensorEvent.values[1]);
-            y.add(sensorEvent.values[1]);
-            zText.setText("Z: " + sensorEvent.values[2]);
-            z.add(sensorEvent.values[2]);
-            count += 1;
-            previous_time_stamp=currentTimeMillis;
-            System.out.println(currentTimeMillis);
+
+        // Trying to add a sample every ~100ms
+        if(last_time_stamp_added==0){
+            last_time_stamp_added=currentTimeMillis;
+            addSensorData(sensorEvent.values[0], sensorEvent.values[1], sensorEvent.values[2]);
+        }
+        else if(currentTimeMillis-last_time_stamp_added<100){
+            last_time_stamp = currentTimeMillis;
+            temp_x = sensorEvent.values[0];
+            temp_y = sensorEvent.values[1];
+            temp_z = sensorEvent.values[2];
+        }
+        else{
+            //System.out.println(0);
+            //System.out.println(Math.abs(last_time_stamp-last_time_stamp_added-100));
+            //System.out.println(currentTimeMillis-last_time_stamp_added-100);
+            if(Math.abs(last_time_stamp-last_time_stamp_added-100) <= currentTimeMillis-last_time_stamp_added-100){
+                last_time_stamp_added=last_time_stamp;
+                addSensorData(temp_x, temp_y, temp_z);
+                last_time_stamp=currentTimeMillis;
+                temp_x = sensorEvent.values[0];
+                temp_y = sensorEvent.values[1];
+                temp_z = sensorEvent.values[2];
+            }
+            else{
+                last_time_stamp_added=currentTimeMillis;
+                addSensorData(sensorEvent.values[0], sensorEvent.values[1], sensorEvent.values[2]);
+            }
         }
     }
 
